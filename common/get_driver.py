a070e37dd9a31d35292a514c4652d1071e63cbbd
阳靖康 <yjk01597132@alibaba-inc.com>
2023-04-28 14:14:35 +0800

    优化无界面启动截屏方法

--- common/get_driver.py
+++ common/get_driver.py
@@ -16,6 +16,11 @@ class GetDriver:
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')  # 无浏览器模式
         options.add_argument('--start-fullscreen')
+        options.add_argument('--disable-gpu')  # 禁用GPU加速
+        options.add_argument('--start-maximized')  # 浏览器最大化
+        # options.add_argument('--window-size=1280x1024')  # 设置浏览器分辨率（窗口大小）
+        options.add_argument('--disable-infobars')  # 禁用浏览器正在被自动化程序控制的提示
+        options.add_argument('log-level=3')
         return options
 
     # 获取driver
@@ -31,7 +36,8 @@ class GetDriver:
                 self.driver = webdriver.Firefox(options=options)
             if browser == 'Edge':
                 self.driver = webdriver.Edge(options=options)
-            self.driver.maximize_window()
+            self.driver.set_window_size(1920, 1080) # 无界面模式使用
+            #self.driver.maximize_window() # 有界面使用方法
             self.driver.get(url)
             log.info(f'=================获取url:{url}成功=================')
         return self.driver
